7fc5a445bcec2512903d572ce548c2b1831d5263
Abhijitsarker <abhijitsarker03@gmail.com>
2023-01-19 09:11:51 +0600

    first commit

---
+++ submission.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Plugin Name:       Submission
+ * Plugin URI:        https://github.com/AbhijitSarker
+ * Description:       Submit Your thoughts and get a email Reply.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Abhijit Sarker
+ * Author URI:        https://github.com/AbhijitSarker
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       submission
+ * Domain Path:       /languages
+ */
+
+if (!defined('ABSPATH')) {
+    die;
+}

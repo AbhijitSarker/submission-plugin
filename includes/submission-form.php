e55d831d20587c6b2463d0b3db52a24d5e688d32
Abhijitsarker <abhijitsarker03@gmail.com>
2023-01-21 09:36:54 +0600

    ajax and rest api end point set

--- includes/submission-form.php
+++ includes/submission-form.php
@@ -6,3 +6,49 @@ function show_submission_form()
 {
     require_once MY_PLUGIN_PATH . 'templates/submission-form.php';
 }
+
+add_action('rest_api_init', 'create_rest_endpoint');
+
+function create_rest_endpoint()
+{
+    register_rest_route('v1/submission-form', 'submit', array(
+        'methods' => 'POST',
+        'callback' => 'handle_enquiry'
+    ));
+}
+
+function handle_enquiry($data)
+{
+    $params = $data->get_params();
+
+    if (!wp_verify_nonce($params['_wpnonce'], 'wp_rest')) {
+        return new WP_REST_Response('Message not sent', 422);
+    }
+
+    unset($params['_wpnonce']);
+    unset($params['_wp_http_referer']);
+
+
+    //send email message
+    $headers = [];
+
+    $admin_email = get_bloginfo('admin_email');
+    $admin_name = get_bloginfo('name');
+
+    $headers[] =  "From: {$admin_name} <{$admin_email}>";
+    $headers[] =  "Reply-to: {$params['name']} <{$params['email']}>";
+    $headers[] =  "Content-type: text/html";
+
+    $subject = "New enquiry from {$params['name']}";
+
+    $message = '';
+    $message .= "<h1>message has been sent from {$params['name']}</h1></br>";
+
+    foreach ($params as $label => $value) {
+        $message .= '<strong>' . ucfirst($label) . '<strong>' . ':' . $value . '</br>';
+    }
+
+    wp_mail($admin_email, $subject, $message, $headers);
+
+    return new WP_REST_Response('Message was sent', 200);
+}
--- templates/submission-form.php
+++ templates/submission-form.php
@@ -1,1 +1,46 @@
-<h1>heello</h1>
+<form id="enquiry_form" action="">
+
+    <?php wp_nonce_field('wp_rest'); ?>
+
+    <label for="">Name</label><br>
+    <input type="text" name="name"> <br>
+
+    <label for="">Email</label><br>
+    <input type="text" name="email"> <br>
+
+    <label for="">Phone</label><br>
+    <input type="text" name="phone"> <br>
+
+    <label for="">Your Message</label><br>
+    <textarea name="message" id="" cols="10" rows="4"></textarea>
+
+    <button type="submit">Submit</button>
+</form>
+
+
+<script>
+    jQuery(document).ready(function($) {
+
+        $("#enquiry_form").submit(function(event) {
+
+            event.preventDefault();
+
+            var form = $(this);
+
+            console.log(form.serialize());
+
+            $.ajax({
+
+                type: "POST",
+                url: "<?php echo get_rest_url(null, 'v1/submission-form/submit'); ?>",
+                data: form.serialize()
+
+
+
+            })
+        });
+
+
+
+    });
+</script>
